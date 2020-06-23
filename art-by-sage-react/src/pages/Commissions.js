e5204a8a1c417861dff4ed63f33a4a8437a18547
Brian Kozub <ticet11@gmail.com>
2020-06-22 20:45:55 -0600

    Skeleton of the commission page.

--- art-by-sage-react/src/pages/Commissions.js
+++ art-by-sage-react/src/pages/Commissions.js
@@ -1,86 +1,53 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Commissions() {
-    const { register, handleSubmit, errors } = useForm();
-
-    const onSubmit = (data) => {
-        console.log(data);
-    };
     return (
-        <div className="contact-form-container">
-            <form
-                className="contact-form"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <input
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    ref={register({ required: "What's your name?!" })}
+        <div>
+            <div className="commission-intro">
+                <img
+                    src="https://via.placeholder.com/150"
+                    alt=""
+                    className="portrait"
                 />
-                {errors.name && (
-                    <p className="error-message">
-                        {
-                            <FontAwesomeIcon icon="exclamation-triangle" />
-                        }
-                        {errors.name.message}
-                    </p>
-                )}
-                <input
-                    type="email"
-                    placeholder="E-Mail"
-                    name="email"
-                    ref={register({
-                        required:
-                            "I need your e-mail so I can respond!",
-                    })}
-                />
-                {errors.email && (
-                    <p className="error-message">
-                        {
-                            <FontAwesomeIcon icon="exclamation-triangle" />
-                        }
-                        {errors.email.message}
-                    </p>
-                )}
-                <input
-                    type="text"
-                    placeholder="Subject"
-                    name="subject"
-                    ref={register({
-                        required:
-                            "Let me know what this is all about!",
-                    })}
-                />
-                {errors.subject && (
-                    <p className="error-message">
-                        {
-                            <FontAwesomeIcon icon="exclamation-triangle" />
-                        }
-                        {errors.subject.message}
-                    </p>
-                )}
-                <textarea
-                    name="message"
-                    cols="30"
-                    rows="10"
-                    placeholder="How can I help you?"
-                    ref={register({
-                        required: "What do you need to tell me?",
-                    })}
-                ></textarea>
-                {errors.message && (
-                    <p className="error-message">
-                        {
-                            <FontAwesomeIcon icon="exclamation-triangle" />
-                        }
-                        {errors.message.message}
-                    </p>
-                )}
-                <button>Send</button>
-            </form>
+                <h1>Hello!</h1>
+                <h2>Thanks for being here.</h2>
+                <p>Ask me about my very cool art commissions.</p>
+            </div>
+            <div className="prices">
+                <div className="commission-type">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt=""
+                        className="price-image"
+                    />
+                    <h2>Head</h2>
+                    <p>$0.00 per person</p>
+                </div>
+                <div className="commission-type">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt=""
+                        className="price-image"
+                    />
+                    <h2>Head</h2>
+                    <p>$0.00 per person</p>
+                </div>
+                <div className="commission-type">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt=""
+                        className="price-image"
+                    />
+                    <h2>Head</h2>
+                    <p>$0.00 per person</p>
+                </div>
+            </div>
+            <div className="request">
+                <h2>Know what you want?</h2>
+                <h2>Wanna discuss an idea?</h2>
+                <h2>Send me an</h2>
+                <button>email</button>
+            </div>
         </div>
     );
 }
