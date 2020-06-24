0fb73f4b47e32344dee17793d8924d402e2ea7c7
Brian Kozub <ticet11@gmail.com>
2020-06-24 16:45:02 -0600

    added passwords are hashed.

--- art-by-sage-database/models/user.js
+++ art-by-sage-database/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -10,5 +11,24 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
 });
+
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password")) return next();
+    bcrypt.hash(this.password, 12, (err, passwordHash) => {
+        if (err) return next(err);
+        this.password = passwordHash;
+        next();
+    });
+});
+
+userSchema.methods.comparePassword = function (password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        else {
+            if (!isMatch) return cb(null, isMatch);
+            return cb(null, this);
+        }
+    });
+};
 
 module.exports = mongoose.model("User", userSchema);
--- art-by-sage-database/routes/users.js
+++ art-by-sage-database/routes/users.js
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
     const user = new User({
         username: req.body.username,
         password: req.body.password,
-    });
+    })
 
     try {
         const newUser = await user.save();
