1d2c0663e0d9a7439907898b5d6b889f1e314747
Brian Kozub <ticet11@gmail.com>
2020-06-24 14:35:16 -0600

    Commissions page text content added.

--- art-by-sage-react/src/pages/Commissions.js
+++ art-by-sage-react/src/pages/Commissions.js
@@ -9,9 +9,12 @@ export default function Commissions() {
                     alt=""
                     className="portrait"
                 />
-                <h1>Hello!</h1>
-                <h2>Thanks for being here.</h2>
-                <p>Ask me about my very cool art commissions.</p>
+                <h1>Commissions</h1>
+                <p>
+                    Thank you so much for beings interested in my art!
+                    I’d love to create for you. Let’s talk through
+                    pricing and move along to the process.
+                </p>
             </div>
             <div className="prices">
                 <div className="commission-type">
@@ -20,8 +23,29 @@ export default function Commissions() {
                         alt=""
                         className="price-image"
                     />
-                    <h2>Head</h2>
-                    <p>$0.00 per person</p>
+                    <h2>Headshots</h2>
+                    <p>
+                        $20 for first subject and $8 for each
+                        additional subject.
+                        <br />
+                        These are great as social media icons but can
+                        be used for anything!
+                    </p>
+                </div>
+                <div className="commission-type">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt=""
+                        className="price-image"
+                    />
+                    <h2>Half Body</h2>
+                    <p>
+                        $25 for first subject and $10 for each
+                        additional subject
+                        <br />
+                        Need a drawing of just the waist up? Perfect.
+                        This half body is just for you.
+                    </p>
                 </div>
                 <div className="commission-type">
                     <img
@@ -30,7 +54,13 @@ export default function Commissions() {
                         className="price-image"
                     />
                     <h2>Head</h2>
-                    <p>$0.00 per person</p>
+                    <p>
+                        $30 for first subject and $12 for each
+                        additional subject
+                        <br />
+                        Whether the subject is standing or dancing or
+                        jumping, we’ll get it in its full glory.
+                    </p>
                 </div>
                 <div className="commission-type">
                     <img
@@ -38,15 +68,54 @@ export default function Commissions() {
                         alt=""
                         className="price-image"
                     />
-                    <h2>Head</h2>
-                    <p>$0.00 per person</p>
+                    <h2>Pets</h2>
+                    <p>
+                        $10 each.
+                        <br />
+                        Pets are part of the family and deserve to be
+                        included in your family portrait!
+                    </p>
+                </div>
+                <div className="commission-type">
+                    <img
+                        src="https://via.placeholder.com/150"
+                        alt=""
+                        className="price-image"
+                    />
+                    <h2>Etc.</h2>
+                    <p>
+                        If you’re interested in adding lettering,
+                        flora or fauna, or simply have requests or
+                        questions, please bring those up when you
+                        contact me! I want to match your vision as
+                        closely as possible.
+                    </p>
                 </div>
             </div>
+            <div>
+                <h1>How Commissions Work</h1>
+                <p>
+                    Once you’ve contacted me and we’ve discussed your
+                    ideas, we have a detailed plan, and payment has
+                    been sent, I’ll start on the preliminary sketch.
+                    Once that’s finished, I’ll send it to you. If
+                    anything needs to be edited, this would be the
+                    time to let me know. I’ll edit the sketch up to
+                    two times. I’ll do the line art and coloring.
+                    Before I add finishing touches, I’ll have you look
+                    over it once more and will edit the colors one
+                    time if need be. Once I get the okay, I’ll
+                    complete the portrait and email you the full file
+                    of both the finished piece and the line art (so
+                    you can print it out and color it). And hooray!!
+                    You have a custom art work.
+                </p>
+            </div>
             <div className="request">
-                <h2>Know what you want?</h2>
-                <h2>Wanna discuss an idea?</h2>
-                <h2>Send me an</h2>
-                <button>email</button>
+                <h2>
+                    Ready to commission me? Send me an email! Can’t
+                    wait to hear from you!
+                </h2>
             </div>
         </div>
     );
