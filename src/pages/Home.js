4ad5b0cfdd502715fb67b052a7a013ae12307c97
Brian Kozub <ticet11@gmail.com>
2020-06-26 12:36:41 -0600

    reinstalled node modules.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ export default function Home() {
                         you’d like.
                     </p>
                 </div>
+                
                 <div
                     style={{
                         background:
